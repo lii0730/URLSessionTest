81a57c384d4edc05a11507e40e8c1ab45a88484b
LeeHsss <lii0730@icloud.com>
2021-07-21 20:53:03 +0900

    Initial Commit

---
+++ URLSessionTest/URLSessionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSessionTestApp.swift
+//  URLSessionTest
+//
+//  Created by LeeHsss on 2021/07/21.
+//
+
+import SwiftUI
+
+@main
+struct URLSessionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
